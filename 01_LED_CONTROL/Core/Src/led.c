9b68dee14d3619bd17b266805b689b589c736f70
kccistc <kccistc@LAPTOP-F74GHAFM>
2024-09-23 16:33:55 +0900

    new

---
+++ 01_LED_CONTROL/Core/Src/led.c
@@ -0,0 +1,57 @@
+#include "led.h"
+
+void led_all_on(void);
+void led_all_off(void);
+void led_up_on(void);
+void led_down_on(void);
+void led_main(void);
+
+
+void led_all_on(void)
+{
+//	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7, 1);
+	HAL_GPIO_WritePin(GPIOB, 0xff, 1);
+}
+
+void led_all_off(void)
+{
+//	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7, 0);
+	HAL_GPIO_WritePin(GPIOB, 0xff, 0);
+}
+
+void led_up_on(void)
+{
+	for (int i = 0; i < 8; i++)
+	{
+		led_all_off();
+		HAL_GPIO_WritePin(GPIOB, 0x01 << i, 1);
+		HAL_Delay(200);
+	}
+	led_all_off();
+	HAL_Delay(200);
+}
+
+void led_down_on(void)
+{
+	for (int i = 0; i < 8; i++)
+	{
+		led_all_off();
+		HAL_GPIO_WritePin(GPIOB, 0x80 >> i, 1);
+		HAL_Delay(200);
+	}
+	led_all_off();
+	HAL_Delay(200);
+}
+
+void led_main(void)
+{
+	while(1)
+	{
+		led_all_on();
+		HAL_Delay(500);
+		led_all_off();
+		HAL_Delay(500);
+		led_up_on();
+		led_down_on();
+	}
+}
