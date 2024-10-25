f077f89cf2f5cecdf424047d24a182f55af0298c
Hello-Jin-World <daniel45598190@gmail.com>
2024-10-25 16:28:39 +0900

    cal

--- STM32F429ZI_KEYPAD/Core/Src/calculater.c
+++ STM32F429ZI_KEYPAD/Core/Src/calculater.c
@@ -1,2 +1,127 @@
 #include "keypad.h"
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define STACK_SIZE 100
+
+typedef double element;
+
+typedef struct {
+    element stack[STACK_SIZE];
+    int top;
+} StackType;
+
+void init(StackType* s) {
+    s->top = -1;
+}
+
+int is_empty(StackType* s) {
+    return (s->top == -1);
+}
+
+int is_full(StackType* s) {
+    return (s->top == (STACK_SIZE - 1));
+}
+
+void push(StackType* s, element item) {
+    if (is_full(s)) {
+        fprintf(stderr, "Stack overflow error\n");
+        return;
+    }
+    else {
+        s->stack[++(s->top)] = item;
+    }
+}
+
+element peek(StackType* s) {
+    if (is_empty(s)) {
+        fprintf(stderr, "Stack underflow error\n");
+        exit(1);
+    }
+    else {
+        return s->stack[s->top];
+    }
+}
+
+element pop(StackType* s) {
+    if (is_empty(s)) {
+        fprintf(stderr, "Stack underflow error\n");
+        exit(1);
+    }
+    else {
+        return s->stack[(s->top)--];
+    }
+}
+
+int prec(char op) {
+    switch (op) {
+    case '(': case ')': return 0;
+    case '+': case '-': return 1;
+    case '*': case '/': return 2;
+    }
+    return -1;
+}
+
+void in_to_post(char* infix, char* postfix) {
+    StackType s;
+    init(&s); // Declare and initialize stack
+
+    while (*infix != '\0') { // Repeat until NULL value in input string
+        if (*infix == ' ') {
+            infix++; // Ignore ' ' space
+        }
+        else if (*infix == '(') {
+            push(&s, *infix);
+            infix++;
+        }
+        else if (*infix == ')') {
+            while (peek(&s) != '(') {
+                *postfix++ = pop(&s);
+                *postfix++ = ' ';
+            }
+            pop(&s); // Remove '('
+            infix++;
+        }
+        else if (*infix == '+' || *infix == '-' || *infix == '*' || *infix == '/') {
+            while (!is_empty(&s) && (prec(*infix) <= prec(peek(&s)))) {
+                *postfix++ = pop(&s);
+                *postfix++ = ' ';
+            }
+            push(&s, *infix);
+            infix++;
+        }
+        else if (*infix >= '0' && *infix <= '9') {
+            do {
+                *postfix++ = *infix++;
+            } while (*infix >= '0' && *infix <= '9');
+            *postfix++ = ' ';
+        }
+        else {
+            infix++;
+        }
+    }
+    while (!is_empty(&s)) {
+        *postfix++ = pop(&s);
+        *postfix++ = ' ';
+    }
+    postfix--;
+    *postfix = '\0';
+}
+
+int calculator_stm(void)
+{
+    char input[100];
+    char postfix[100];
+
+    printf("Enter expression: ");
+    fgets(input, sizeof(input), stdin);
+
+    input[strcspn(input, "\n")] = '\0';
+
+    in_to_post(input, postfix);
+    printf("Postfix expression: %s\n", postfix);
+
+    return 0;
+}
